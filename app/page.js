09f64c617367d8b4052c77ee0950e1bafc5da86c
Shack <g0972222165@gmail.com>
2023-12-30 09:40:26 +0800

    Page

--- app/page.js
+++ app/page.js
@@ -89,7 +89,7 @@ import Input from '@mui/material/Input';
       <div className="w-full">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert mx-auto"
-          src="/next.svg"
+          src="./next.svg"
           alt="Next.js Logo"
           width={180}
           height={37}
